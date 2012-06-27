b6501bb3db8de774f4b109d8f405f0153ab805d4
Pratik  Dam <pdam.2010@gmail.com>
2012-06-27 13:03:10 +0000

    default   cookbooks    to resolve dependency issues  .Would remove later

--- cookbooks/cmsapp/metadata.rb
+++ cookbooks/cmsapp/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email ""
 license          "Apache 2.0"
 description      "Deploys and configures a variety of cmsapps defined from databag 'apps'"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "0.0.1"
 recipe           "cmsapp", "Loads cmsapp databags and selects recipes to use"
 recipe           "cmsapp::mod_php_apache2", "Sets up a deployed PHP cmsapp as a mod_php virtual host in Apache2"
 recipe           "cmsapp::php_pear_module_install", "Deploys a PHP cmsapp specified in a data bag with the deploy_revision resource"
