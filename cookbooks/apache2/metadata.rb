0cc63212330944a38ef425b29b77e67c47175222
Pratik  Dam <pdam.2010@gmail.com>
2012-06-27 10:51:04 +0000

    added   comments

--- cookbooks/apache2/metadata.rb
+++ cookbooks/apache2/metadata.rb
@@ -1,5 +1,5 @@
-maintainer        "Opscode, Inc."
-maintainer_email  "cookbooks@opscode.com"
+maintainer        "Trimmed   by Pratik"
+maintainer_email  "pdam@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures all aspects of apache2 using Debian style symlinks with helper definitions"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
